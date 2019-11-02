8981b52822aaf555203d7c54827a8606c0ee2c12
vladimir-drachuk <vvdra4@gmail.com>
2019-11-02 17:59:50 +0300

    feat: add caps lock function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
+import keyStorage from './storage';
+
 const ul = document.createElement('ul');
 const textarea = document.createElement('textarea');
 document.body.appendChild(textarea);
 document.body.appendChild(ul);
-
-
-const keyStorage = {
-  code: ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9',
-    'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO',
-    'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH',
-    'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN',
-    'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight',
-    'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
-  ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-    'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'xz!!',
-    'caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
-    'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'shift',
-    'ctrl', 'win', 'alt', ' ', 'alt', '◀', '▼', '▶', 'ctrl'],
-  en: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-    'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'xz!!',
-    'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'xz', 'enter',
-    'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
-    'ctrl', 'win', 'alt', ' ', 'alt', '◀', '▼', '▶', 'ctrl'],
-  link : [],
-};
-
+//const shiftArray = [];
 
 class Box {
   constructor(rutext, entext, code) {
@@ -43,30 +24,126 @@ class Box {
   }
 }
 
+function addClass() {
+  button.elem.classList.add('button', keyStorage.code[i].toLocaleLowerCase());
+  if (button.elem.textContent === 'tab'
+  || button.elem.textContent === 'shift'
+  || button.elem.textContent === 'caps lock'
+  || button.elem.textContent === 'backspace'
+  || button.elem.textContent === 'alt'
+  || button.elem.textContent === 'enter'
+  || button.elem.textContent === 'ctrl'
+  || button.elem.textContent === 'win') {
+    button.elem.classList.add('specialButton');
+  }  
+  if (button.elem.textContent === '1'
+  || button.elem.textContent === '2'
+  || button.elem.textContent === '3'
+  || button.elem.textContent === '4'
+  || button.elem.textContent === '5'
+  || button.elem.textContent === '6'
+  || button.elem.textContent === '7'
+  || button.elem.textContent === '8'
+  || button.elem.textContent === '9'
+  || button.elem.textContent === '0'
+  || button.elem.textContent === '-'
+  || button.elem.textContent === '='
+  || button.elem.textContent === '`'
+  || button.elem.textContent === 'ё'
+  || button.elem.textContent === 'Ё') {
+    button.elem.classList.add('firstLine');
+  }
+}
+
+function CapsLock() {
+  if (localStorage.capslock === null || localStorage.capslock === 'false') {
+    localStorage.setItem('capslock', true);
+    keyStorage.link[keyStorage.code.indexOf('CapsLock')].classList.add('click');
+    keyStorage.code.forEach((item, i) => {
+      if (keyStorage.link[i].classList.contains('specialButton')) {
+        keyStorage.link[i].textContent = keyStorage.link[i].textContent.toLowerCase();
+      } else {
+        keyStorage.link[i].textContent = keyStorage.link[i].textContent.toUpperCase();
+      }
+    });
+  } else {
+    localStorage.setItem('capslock', false);
+    keyStorage.link[keyStorage.code.indexOf('CapsLock')].classList.remove('click');
+    keyStorage.code.forEach((item, i) => {
+      keyStorage.link[i].textContent = keyStorage.link[i].textContent.toLowerCase();
+    });
+  }
+}
+
+function Shift() {
+  keyStorage.firstline.length = 0;
+  keyStorage.shiftArray.length = 0;
+  console.log(keyStorage.firstline);
+  keyStorage.link.forEach((item, i) => {
+    if (keyStorage.link[i].classList.contains('firstLine')) {
+      keyStorage.shiftArray.push(keyStorage.link[i]);
+      keyStorage.firstline.push(keyStorage.link[i].textContent);
+    }
+  });
+  console.log(keyStorage.shiftArray);
+  console.log(keyStorage.firstline);
+  keyStorage.shiftArray.forEach((item, i) => {
+    keyStorage.shiftArray[i].textContent = keyStorage.firstlineS[i];
+  });
+}
 
 for (let i = 0; i < 63; i += 1) {
   const button = new Box(keyStorage.ru[i], keyStorage.en[i], keyStorage.code[i]);
   button.setText();
   ul.appendChild(button.elem);
-  button.elem.classList.add('button', keyStorage.code[i].toLocaleLowerCase());
+  addClass();
   keyStorage.link.push(button.elem);
 }
-console.log(keyStorage.class)
+
 document.addEventListener('keydown', (event) => {
   event.preventDefault();
+  keyStorage.link[keyStorage.code.indexOf(event.code)].classList.add('click');
   if (event.code === 'Backspace') {
     const str = textarea.value.substr(0, textarea.value.length - 1);
     textarea.value = str;
-  } else {
-    textarea.value += keyStorage.en[keyStorage.code.indexOf(event.code)];
+    return;
   }
-  keyStorage.link[keyStorage.code.indexOf(event.code)].classList.add('click');
+  if (event.code === 'Tab') {
+    textarea.value += '   ';
+    return;
+  }
+  if (event.code === 'CapsLock') {
+    CapsLock();
+    return;
+  }
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    Shift();
+    return;
+  }
+  if (event.code === 'ControlLeft' || event.code === 'ControlRight') {
+    return;
+  }
+  if (event.code === 'ControlLeft' || event.code === 'ControlRight') {
+    return;
+  }
+  if (event.code === 'AltLeft' || event.code === 'AltRight') {
+    return;
+  }
+  if (event.code === 'Enter') {
+    textarea.value += '\n';
+    return;
+  }
+  textarea.value += keyStorage.link[keyStorage.code.indexOf(event.code)].textContent;
 });
 
 document.addEventListener('keyup', (event) => {
+  if (event.code === 'CapsLock') return;
   keyStorage.link[keyStorage.code.indexOf(event.code)].classList.remove('click');
+  keyStorage.shiftArray.forEach((item, i) => {
+    keyStorage.shiftArray[i].textContent = keyStorage.firstline[i];
+  });
 });
 
-document.addEventListener('mousedown', (event) => {
+ul.addEventListener('mousedown', (event) => {
   textarea.value += keyStorage.en[keyStorage.code.indexOf(event.code)];
 });
---
+++ src/storage.js
@@ -0,0 +1,24 @@
+const keyStorage = {
+  code: ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9',
+    'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO',
+    'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH',
+    'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN',
+    'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight',
+    'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+  ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+    'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'xz!!',
+    'caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
+    'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'shift',
+    'ctrl', 'win', 'alt', ' ', 'alt', '◀', '▼', '▶', 'ctrl'],
+  en: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+    'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'xz!!',
+    'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'xz', 'enter',
+    'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'shift',
+    'ctrl', 'win', 'alt', ' ', 'alt', '◀', '▼', '▶', 'ctrl'],
+  firstlineS: ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'],
+  firstline: [],
+  link: [],
+  shiftArray: [],
+};
+
+export default keyStorage;
